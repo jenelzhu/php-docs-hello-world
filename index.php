4b0310d40369cf897967ee1dd0c8e4927f2564ce
jenelzhu <106546454+jenelzhu@users.noreply.github.com>
2024-04-26 20:05:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Microsoft Cognitive Services Speech SDK JavaScript Transcription Quickstart</title>
+  <title>Speech Transcription</title>
   <meta charset="utf-8" />
 </head>
 <body style="font-family:'Helvetica Neue',Helvetica,Arial,sans-serif; font-size:13px;">
   <div id="warning">
-    <h1 style="font-weight:500;">Speech Recognition Speech SDK not found (microsoft.cognitiveservices.speech.sdk.bundle.js missing).</h1>
+    <h1 style="font-weight:500;">Speech Recognition is starting).</h1>
   </div>
   
   <div id="content" style="display:none">
